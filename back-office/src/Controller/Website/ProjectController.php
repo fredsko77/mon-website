0ae2a419594dfcc261bce1f054d3b3e9f4672dd3
AGATHE Frédérick <fagathe@meilleurtaux.com>
2023-01-31 21:59:28 +0100

    feat(templates realisations)

--- back-office/src/Controller/Website/ProjectController.php
+++ back-office/src/Controller/Website/ProjectController.php
@@ -1,12 +1,19 @@
 <?php
 namespace App\Controller\Website;
+
+use App\Service\ProjectService;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 #[Route('/mes-realisations', name: 'website_project_')]
 class ProjectController extends AbstractController 
 {
+
+    public function __construct(
+        private ProjectService $service
+    ) {}
 
     #[Route('/{slug}-{id}', name: 'show', requirements: ['id' => '\d+', 'slug' => '[a-z0-9\-]*'], methods: ['GET'])]
     public function show():Response 
@@ -15,9 +22,9 @@ class ProjectController extends AbstractController
     }
 
     #[Route('', name: 'index', methods: ['GET'])]
-    public function index(): Response 
+    public function index(Request $request): Response 
     {
-        return $this->render('');
+        return $this->render('website/project/index.html.twig', $this->service->search($request));
     }
 
 }
